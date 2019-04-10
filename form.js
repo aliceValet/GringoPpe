389d0221ea48df2c8208388e07e720d729cc82b3
Alice VALET <alice_valet@orange.fr>
2019-04-10 16:37:18 +0200

    json done. demo advanced.

--- form.js
+++ form.js
@@ -1,9 +1,20 @@
-function(){
+function displayProut(){
 
-
+	var toto=document.getElementById('total').value;
+	var priorite = document.getElementById('prio').value;
+	document.getElementById("blabla").innerHTML = "<img class='prout' src='prout_car.png' alt='oupsi'/><p>Chargement ...</p>";
+	var requestURL = './data.json';
+	var request = new XMLHttpRequest();
+	request.open('GET', requestURL);
+	request.responseType = 'json';
+	request.send();
+	request.onload = function() {
+		var answer = request.response;
+		inputData(answer);
+	}
+	function inputData(jsonObj){
+		jsonObj.nbVehic = toto;
+		jsonObj.nbPrio = priorite;
+		console.log(jsonObj);
+	}
 }
-$(document).ready(function(){
-  $("button").click(function(){
-    $("<span>Hello world!</span>").insertAfter("p");
-  });
-});
